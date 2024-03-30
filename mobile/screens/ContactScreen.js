bc5bdacf88f8a035ade9d809fe11132ab25f41c8
thanhthang2808 <thanhthang2808@gmail.com>
2024-03-30 20:22:06 +0700

    Update UI ContactScreen and ProfileScreen

--- mobile/screens/ContactScreen.js
+++ mobile/screens/ContactScreen.js
@@ -1,12 +1,155 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, TouchableOpacity, FlatList, Image } from "react-native";
+import Feather from "react-native-vector-icons/Feather";
 
 export default function ContactScreen() {
+  const [activeTab, setActiveTab] = useState("friends");
+
+  const friendsData = [
+    { id: 1, name: "Dương Thế Ngọc", avatar: require("../assets/girl.png") },
+    { id: 2, name: "Võ Quốc Huy", avatar: require("../assets/avt.jpg") },
+    { id: 3, name: "Nguyễn Việt Bắc", avatar: require("../assets/men.png") },
+  ];
+
+  const invitationsData = [
+    { id: 1, name: "Vũ Lan Tường", avatar: require("../assets/girl.png") },
+    { id: 2, name: "Nguyễn Phương Cường", avatar: require("../assets/men.png") },
+    { id: 3, name: "Nguyễn Quốc Khôi", avatar: require("../assets/senior.png") },
+  ];
+
+  const renderFriendItem = ({ item }) => (
+    <View style={styles.friendItem}>
+      <Image source={item.avatar} style={styles.avatar} />
+      <View style={styles.friendInfo}>
+        <Text>{item.name}</Text>
+        <View style={styles.contactButtons}>
+          <TouchableOpacity style={styles.contactButton}>
+            <Feather name="phone" size={20} color="grey" />
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.contactButton}>
+            <Feather name="message-square" size={20} color="grey" />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+
+  const renderInvitationItem = ({ item }) => (
+    <View style={styles.friendItem}>
+      <Image source={item.avatar} style={styles.avatar} />
+      <View style={styles.friendInfo}>
+        <Text>{item.name}</Text>
+        <View style={styles.friendRequestButtons}>
+          <TouchableOpacity style={[styles.friendRequestButton, { backgroundColor: "#B5EAD7" }]}>
+            <Text>Đồng ý</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={[styles.friendRequestButton, { backgroundColor: "#FF9AA2" }]}>
+            <Text>Từ chối</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
+
   return (
-    <View>
-      <Text>ContactScreen</Text>
+    <View style={styles.container}>
+      <View style={styles.tabContainer}>
+        <TouchableOpacity
+          style={[styles.tab, activeTab === "friends" && styles.activeTab]}
+          onPress={() => setActiveTab("friends")}
+        >
+          <View style={{ flexDirection: "row" }}>
+            <Feather name="users" size={25} color="#444444" />
+            <Text style={styles.tabText}>Danh sách bạn bè</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.tab, activeTab === "invitations" && styles.activeTab]}
+          onPress={() => setActiveTab("invitations")}
+        >
+          <View style={{ flexDirection: "row" }}>
+            <Feather name="user-plus" size={25} color="#444444" />
+            <Text style={styles.tabText}>Lời mời kết bạn</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+      {activeTab === "friends" ? (
+        <FlatList
+          data={friendsData}
+          renderItem={renderFriendItem}
+          keyExtractor={(item) => item.id.toString()}
+        />
+      ) : (
+        <FlatList
+          data={invitationsData}
+          renderItem={renderInvitationItem}
+          keyExtractor={(item) => item.id.toString()}
+        />
+      )}
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 20,
+  },
+  tabContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginBottom: 10,
+  },
+  tab: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+  },
+  activeTab: {
+    borderBottomWidth: 2,
+    borderBottomColor: "#33CCFF",
+  },
+  tabText: {
+    fontSize: 14,
+    fontWeight: "bold",
+    marginLeft: 10,
+    color: "#444444",
+  },
+  friendItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+  },
+  avatar: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    marginRight: 10,
+  },
+  friendInfo: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  contactButtons: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  contactButton: {
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 5,
+    marginLeft: 10,
+  },
+  friendRequestButtons: {
+    flexDirection: "row",
+  },
+  friendRequestButton: {
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 5,
+    marginLeft: 10,
+  },
+});
--- mobile/screens/ProfileScreen.js
+++ mobile/screens/ProfileScreen.js
@@ -1,24 +1,167 @@
-import { Button, StyleSheet, Text, View } from "react-native";
-import React from "react";
-import { useDispatch } from "react-redux";
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Image, Pressable, Modal, TouchableOpacity, ImageBackground } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { removeToken } from "../app/tokenSlice";
+import { Feather } from "@expo/vector-icons";
 
 export default function ProfileScreen({ navigation }) {
-  const dispatch = useDispatch();
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const username = "Dương Thế Huy";
+  const avatar = require("../assets/avt.jpg");
+  const backgroundImage = require("../assets/bg.jpg");
+
+  const handleLogout = async () => {
+    try {
+      await AsyncStorage.removeItem("token");
+      navigation.navigate("Login");
+    } catch (error) {
+      console.log("Error logging out:", error);
+    }
+  };
+
   return (
-    <View style={{ flex: 1, alignItems: "center" }}>
-      <Text>Profile Screen</Text>
-      <Button
-        title="Logout"
-        onPress={() => {
-          AsyncStorage.removeItem("token");
-          dispatch(removeToken());
-          navigation.navigate("Login");
-        }}
-      />
+    <View style={styles.container}>
+      <ImageBackground
+        resizeMode="cover"
+        style={{ width: "100%", height: "55%" }}
+        source={backgroundImage}
+      >
+        <View style={styles.profileContainer}>
+          <TouchableOpacity onPress={() => setModalVisible(true)}>
+            <Image style={styles.avatar} source={avatar} />
+          </TouchableOpacity>
+          <Text style={styles.username}>{username}</Text>
+          <Text style={styles.nickname}>@huydeptrai</Text>
+        </View>
+        <Pressable style={styles.logoutButton}>
+          <Feather name="edit" size={20} color="#444444" />
+        </Pressable>
+        <Modal
+          animationType="fade"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => setModalVisible(false)}
+        >
+          <TouchableOpacity
+            style={styles.modalBackground}
+            activeOpacity={1}
+            onPressOut={() => setModalVisible(false)}
+          >
+            <View style={styles.modalContent}>
+              <Image style={styles.modalAvatar} source={avatar} />
+            </View>
+          </TouchableOpacity>
+        </Modal>
+        <View style={styles.bottomContainer}>
+        <TouchableOpacity style={styles.bottomButton}>
+          <Feather name="shield" size={15} color="#33CCFF" />
+          <Text style={styles.bottomButtonText}>Tài khoản và bảo mật</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.bottomButton}>
+        <Feather name="settings" size={15} color="#33CCFF" />
+          <Text style={styles.bottomButtonText}>Quyền riêng tư</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.bottomButton}>
+        <Feather name="bell" size={15} color="#33CCFF" />
+          <Text style={styles.bottomButtonText}>Thông báo</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.bottomButton}>
+        <Feather name="users" size={15} color="#33CCFF" />
+          <Text style={styles.bottomButtonText}>Danh bạ</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.bottomButton}>
+        <Feather name="bookmark" size={15} color="#33CCFF" />
+          <Text style={styles.bottomButtonText}>Nhật ký</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.bottomButton}>
+        <Feather name="archive" size={15} color="#33CCFF" />
+          <Text style={styles.bottomButtonText}>Sao lưu và ngôn ngữ</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.bottomButton} onPress={handleLogout}>
+        <Feather name="log-out" size={15} color="red" />
+          <Text style={{fontSize: 14, marginLeft: 10, color: "red" }}>Đăng xuất</Text>
+        </TouchableOpacity>
+      </View>
+      </ImageBackground>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  profileContainer: {
+    alignItems: "center",
+    marginTop: 100,
+  },
+  avatar: {
+    width: 120,
+    height: 120,
+    borderRadius: 75,
+    borderWidth: 4,
+    borderColor: "#fff",
+    marginTop: 20,
+  },
+  username: {
+    marginTop: 10,
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#404146",
+  },
+  nickname: {
+    marginTop: 5,
+    fontSize: 12,
+    color: "#777777",
+  },
+  logoutButton: {
+    position: "absolute",
+    top: 20,
+    right: 20,
+    padding: 10,
+    borderRadius: 5,
+  },
+  logoutButtonText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#00ACEE",
+  },
+  modalBackground: {
+    flex: 1,
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalContent: {
+    backgroundColor: "#fff",
+    borderRadius: 10,
+    padding: 20,
+    elevation: 5,
+  },
+  modalAvatar: {
+    width: 300,
+    height: 300,
+    borderRadius: 150,
+  },
+  bottomContainer: {
+    flexDirection: "row",
+    marginTop: 10,
+    flexDirection: "column",
+    justifyContent: "flex-start",
+  },
+  bottomButton: {
+    backgroundColor: "#DDE0E6",
+    paddingVertical: 6,
+    paddingHorizontal: 15,
+    borderRadius: 10,
+    marginVertical: 5,
+    marginHorizontal: 30,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  bottomButtonText: {
+    fontSize: 14,
+    color: "#444444",
+    marginLeft: 10,
+  },
+});
