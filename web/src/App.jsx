3e2406d53606249526f59afeb6d324820a9113ad
VoQuocHuy <voquochuy042@gmail.com>
2024-05-06 10:06:12 +0700

    admin page

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,12 +1,35 @@
 import { ToastContainer } from 'react-toastify'
 import './App.scss'
 import AppRoutes from './routes/AppRoutes'
+import { ColorModeContext, useMode } from './theme';
+import { useState } from 'react';
+import { ThemeProvider } from '@emotion/react';
+import Sidebar from './scenes/global/Sidebar';
+import { CssBaseline } from '@mui/material';
+import Topbar from './scenes/global/TopBar';
+import { Route, Routes } from 'react-router-dom';
+import Dashboard from './scenes/dashboard';
 
 function App() {
-
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
   return (
     <>
-      <AppRoutes />
+      <ColorModeContext.Provider value={colorMode}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <div className="app">
+            {/* <Sidebar isSidebar={isSidebar} /> */}
+            <main className="content">
+              <Topbar setIsSidebar={setIsSidebar} />
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+              </Routes>
+            </main>
+          </div>
+        </ThemeProvider>
+      </ColorModeContext.Provider>
+      {/* <AppRoutes />
       <ToastContainer
         position="top-right"
         autoClose={5000}
@@ -18,7 +41,7 @@ function App() {
         draggable
         pauseOnHover
         theme="light"
-      />
+      /> */}
     </>
   )
 }
